315c9b153dc06b85707f6f349ac00263c3a0d38c
Bartosz Macek <bartoszmacek@gmail.com>
2019-06-28 22:07:22 +0200

    TODO App

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'task-app';
+
+  taskNumber = 0;
+
+  taskList: Array<number> = [];
+
+
+  onAddTask() {
+    this.taskNumber = this.taskNumber + 1;
+    this.taskList.push(this.taskNumber);
+    console.log(this.taskNumber);
+  }
+
+  onRemove() {
+    if (this.taskNumber > 0) {
+      this.taskList.splice(this.taskNumber - 1);
+      this.taskNumber = this.taskNumber - 1;
+    }
+  }
+
+  onClear() {
+    this.taskList = [];
+    this.taskNumber = 0;
+  }
 }
